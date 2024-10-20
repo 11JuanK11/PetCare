a6fac2bc3a004bbfb678b14be04ab2d8d32a77e9
JuanK <camiloalzatebedoya15@gmail.com>
2024-10-20 17:39:43 -0500

    HU-01: Application registration
    Add classes for registering new Clients.

--- src/main/java/pet/care/petcare/service/impl/ClientServiceImpl.java
+++ src/main/java/pet/care/petcare/service/impl/ClientServiceImpl.java
@@ -1,4 +1,54 @@
 package pet.care.petcare.service.impl;
 
-public class ClientServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pet.care.petcare.entity.Client;
+
+import pet.care.petcare.exception.ResourceNotFoundException;
+import pet.care.petcare.repository.IClientRepository;
+import pet.care.petcare.service.UserService;
+
+import java.util.List;
+
+@Service
+public class ClientServiceImpl extends UserService<Client> {
+    @Autowired
+    private IClientRepository clientRepository;
+
+
+    @Override
+    public Client insert(Client entity) throws RuntimeException {
+        if (entity == null) {
+            throw new ResourceNotFoundException("User information is missing");
+        }
+        checkExistence(entity.getUsername());
+        entity.setRole("CLIENT");
+        entity.setPassword(this.cryptPasswordEncoder.encode(entity.getPassword()));
+        return clientRepository.save(entity);
+    }
+
+    @Override
+    public Client findById(Long id) throws RuntimeException {
+        return clientRepository.findById(id).orElseThrow(() -> {
+            throw new ResourceNotFoundException("Client not found by: " + id);
+        });
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return clientRepository.findAll();
+    }
+
+    @Override
+    public Client update(Long id) throws RuntimeException {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) throws RuntimeException {
+        if (!clientRepository.existsById(id)){
+            throw new ResourceNotFoundException("Client not found");
+        }
+        clientRepository.deleteById(id);
+    }
 }
