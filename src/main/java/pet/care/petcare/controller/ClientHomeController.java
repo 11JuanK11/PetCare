9a7405e3acddacdae43b7f8cf8acf93eec7c3ceb
JuanK <camiloalzatebedoya15@gmail.com>
2024-10-23 13:26:22 -0500

    HU-04
    Add new files to indicate upcoming features for clients and clinic staff.

---
+++ src/main/java/pet/care/petcare/controller/ClientHomeController.java
@@ -0,0 +1,44 @@
+package pet.care.petcare.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin-panel")
+public class AdminHomeController {
+
+    @GetMapping("")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String home(){
+        return "indexAdmin";
+    }
+
+    @GetMapping("/medications")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String medicationsManagement(){
+        return "medications";
+    }
+
+    @GetMapping("/medications/edit")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String editMedicationsManagement(){
+        return "editMedication";
+    }
+
+    @GetMapping("/treatments")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String treatmentsManagement(){
+        return "treatments";
+    }
+
+    @GetMapping("/treatments/edit")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String editTreatmentsManagement(){
+        return "editTreatment";
+    }
+
+
+
+}
