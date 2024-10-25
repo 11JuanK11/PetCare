751dca6b83366f430c46b7badccb87ca34377ed7
JuanK <camiloalzatebedoya15@gmail.com>
2024-10-24 22:05:10 -0500

    HU-01
    Add a custom error page for 403 Forbidden and 404 Not Found

--- src/main/java/pet/care/petcare/controller/PublicController.java
+++ src/main/java/pet/care/petcare/controller/PublicController.java
@@ -3,6 +3,8 @@ package pet.care.petcare.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/public")
 @RequiredArgsConstructor
-public class PublicController {
+public class PublicController implements ErrorController {
 
     @GetMapping("/login")
     public String home(HttpServletRequest request){
@@ -22,11 +24,17 @@ public class PublicController {
     }
 
     @GetMapping("/register")
-    public String admin(HttpServletRequest request){
+    public String register(HttpServletRequest request){
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
         }
         return "Register";
+    }
+
+    @GetMapping("/error")
+    public String errorPage(){
+
+        return "error";
     }
 }
