0f3f251078b17b04611ec49c47877251f22f1531
Sara-Castaneda25 <saracastanedaecheverri@gmail.com>
2024-11-04 12:30:02 -0500

    HU-10
    (Fix) frontend medical histories bug

--- src/main/java/pet/care/petcare/repository/IDiagnosticRepository.java
+++ src/main/java/pet/care/petcare/repository/IDiagnosticRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pet.care.petcare.entity.Diagnostic;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IDiagnosticRepository extends JpaRepository<Diagnostic, Long> {
     Optional<Diagnostic> findByRecipeId(Long recipeId);
+    List<Diagnostic> findByMedicalHistoryId(Long medicalHistoryId);
 }
