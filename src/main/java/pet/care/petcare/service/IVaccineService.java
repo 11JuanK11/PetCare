40876413edac67e097190611d5893a027d3f0b6e
Sara-Castaneda25 <saracastanedaecheverri@gmail.com>
2024-11-12 14:02:26 -0500

    HU-09
    Add (backend) vaccine and vaccination cards functionality

---
+++ src/main/java/pet/care/petcare/service/IVaccineService.java
@@ -0,0 +1,12 @@
+package pet.care.petcare.service;
+
+import pet.care.petcare.entity.Vaccine;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IVaccineService {
+    public Vaccine insertVaccine(Vaccine vaccine) throws IllegalArgumentException;
+    public List<Vaccine> getAllVaccines();
+    public Optional<Vaccine> getVaccineById(Long id);
+}
