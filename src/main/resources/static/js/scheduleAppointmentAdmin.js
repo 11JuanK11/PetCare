0e3ea6ff3f9f533da0a68b247a26e0372bdb3ef9
IsaMontoya17 <isabelamontoya2021@gmail.com>
2024-11-13 11:25:11 -0500

    HU-30
    Perf(web): Fix function to not allow scheduling appointments at past hours

--- src/main/resources/static/js/scheduleAppointmentAdmin.js
+++ src/main/resources/static/js/scheduleAppointmentAdmin.js
@@ -80,6 +80,10 @@ document.addEventListener("DOMContentLoaded", function () {
     function loadAppointments() {
         const vetId = veterinarianSelect.value;
         const date = dateSelect.value;
+        const today = new Date();
+        const currentHour = today.getHours();
+        const currentMinute = today.getMinutes();
+        const currentDate = today.toISOString().split("T")[0];
 
         if (vetId && date) {
             fetch(`/rest/appointment/appointments/available?clinicStaffId=${vetId}&date=${date}`)
@@ -112,6 +116,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         const tbody = document.createElement("tbody");
                         appointments.forEach(appointment => {
                             const row = document.createElement("tr");
+                            const [startHour, startMinute] = appointment.startTime.split(':').map(Number);
                             const formattedStartTime = appointment.startTime.substring(0, 5);
                             const formattedEndTime = appointment.endTime.substring(0, 5);
 
@@ -135,7 +140,9 @@ document.addEventListener("DOMContentLoaded", function () {
                                 assignButton.style.backgroundColor = "#95BDFF";
                             };
 
-                            if (!appointment.available) {
+                            const isPastTime = date === currentDate &&
+                                (startHour < currentHour || (startHour === currentHour && startMinute <= currentMinute));
+                            if (!appointment.available || isPastTime) {
                                 timeCell.style.backgroundColor = "#f8d7da";
                                 timeCell.style.color = "#721c24";
                                 actionCell.style.backgroundColor = "#f8d7da";
